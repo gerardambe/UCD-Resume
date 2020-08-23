86a9e41ec40de17f95869d529154a2bcd0ffeb33
gerardambe <gerardambe@yahoo.com>
2020-08-23 20:54:17 +0000

    empliment emailjs on the contact page

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,15 @@
+function sendMail(contactForm){
+    emailjs.send("gmail", "resume", {
+        "from_name": contactForm.fullname.value,
+        "from_email": contactForm.email.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILURE", error);
+        });
+        return false;
+}
